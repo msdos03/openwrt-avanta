d5fdb30128a091088132efe439aba052474382ff
Lu jicong <jiconglu58@gmail.com>
2024-04-04 19:56:44 +0800

    Avanta: remove unneccesary default package kmod-leds-gpio

--- target/linux/avanta/image/generic.mk
+++ target/linux/avanta/image/generic.mk
@@ -2,7 +2,7 @@ define Device/zte_f420-f620
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := F420-F620
   DEVICE_DTS := avanta-f420-f620
-  DEVICE_PACKAGES := kmod-gpio-button-hotplug kmod-leds-gpio kmod-rtc-mv
+  DEVICE_PACKAGES := kmod-gpio-button-hotplug kmod-rtc-mv
   PAGESIZE := 512
   SUBPAGESIZE := 256
   BLOCKSIZE := 16k
@@ -19,7 +19,7 @@ define Device/zte_f460-f660
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := F460-F660
   DEVICE_DTS := avanta-f460-f660
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-leds-gpio kmod-usb-ledtrig-usbport \
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport \
 	kmod-gpio-button-hotplug kmod-rtl8192ce kmod-rtc-mv
   PAGESIZE := 512
   SUBPAGESIZE := 256
@@ -37,7 +37,7 @@ define Device/hqw_hgg420n
   DEVICE_VENDOR := HQW
   DEVICE_MODEL := HGG420N
   DEVICE_DTS := avanta-hgg420n
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-leds-gpio kmod-usb-ledtrig-usbport \
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport \
 	kmod-gpio-button-hotplug kmod-rtl8192ce kmod-rtc-mv
   PAGESIZE := 512
   SUBPAGESIZE := 256
