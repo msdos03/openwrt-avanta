49c51ba9fdab570582c1b35010333b5a4bab8429
Lu jicong <jiconglu58@gmail.com>
2025-04-04 18:26:17 +0800

    replace kirkwood target with avanta files
    In this branch, the target name is still kirkwood
    to use the openwrt package source of kirkwood.

--- target/linux/kirkwood/base-files/lib/upgrade/platform.sh
+++ target/linux/kirkwood/base-files/lib/upgrade/platform.sh
@@ -8,11 +8,6 @@ platform_check_image() {
 	local board="$(board_name)"
 
 	case "$board" in
-	netgear,readynas-duo-v2)
-		# let's store how rootfs is mounted
-		cp /proc/mounts /tmp/mounts
-		return 0
-		;;
 	*)
 		return 0
 		;;
@@ -23,25 +18,6 @@ platform_do_upgrade() {
 	local board="$(board_name)"
 
 	case "$board" in
-	ctera,c200-v1)
-		part=$(find_mtd_part "active_bank")
-
-		if [ -n "$part" ]; then
-			CI_KERNPART="$(strings $part | grep bank)"
-			nand_do_upgrade "$1"
-		else
-			echo "active_bank partition missed!"
-			return 1
-		fi
-		;;
-	iptime,nas1)
-		default_do_upgrade "$1"
-		;;
-	linksys,e4200-v2|\
-	linksys,ea3500|\
-	linksys,ea4500)
-		platform_do_upgrade_linksys "$1"
-		;;
 	*)
 		nand_do_upgrade "$1"
 		;;
