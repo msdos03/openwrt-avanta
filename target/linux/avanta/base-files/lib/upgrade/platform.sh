7470d6d50cc336226cc64bf570aa7ba1701214c9
Lu jicong <jiconglu58@gmail.com>
2025-04-19 21:24:42 +0800

    avanta: sync platform.sh with latest one

--- target/linux/avanta/base-files/lib/upgrade/platform.sh
+++ target/linux/avanta/base-files/lib/upgrade/platform.sh
@@ -1,10 +1,17 @@
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_BIN='fw_printenv fw_setenv strings'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
+PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
 platform_check_image() {
-	return 0
+	local board="$(board_name)"
+
+	case "$board" in
+	*)
+		return 0
+		;;
+	esac
 }
 
 platform_do_upgrade() {
